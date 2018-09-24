bb1375c4461bbe647e89fc0ff79e1763315e5cfc
Augusto Queirantes <augusto_queirantes@outlook.com>
2018-09-24 07:54:51 -0300

    Resolve eslint on github_data.js

--- src/github/github_data.js
+++ src/github/github_data.js
@@ -1,7 +1,7 @@
-const octokit = require('@octokit/rest')()
+const octokit = require('@octokit/rest')();
 const path = require('path');
 
-const githubAuthenticationFile = require(path.resolve( __dirname, "../github/github_authentication.js"));
+const githubAuthenticationFile = require(path.resolve( __dirname, '../github/github_authentication.js'));
 
 const githubAuthentication = new githubAuthenticationFile();
 
@@ -9,17 +9,12 @@ githubAuthentication.authenticate(octokit);
 
 class githubData {
   getPullRequestParsedData(pullRequestData, changedFilesData) {
-    console.log(`pull_request_data: ${pullRequestData}`);
-    console.log(`changedFilesData: ${changedFilesData}`);
-
     const { body: { pull_request, repository } } = pullRequestData;
-    console.log(`pull_request: ${pull_request}`);
-    console.log(`repository: ${repository}`);
 
-    let json =  {
+    return  {
       owner: pull_request.head.repo.owner.login,
       repo: repository.name,
-      number: number,
+      number: pull_request.number,
       title: pull_request.title,
       user: pull_request.user.login,
       created_at: pull_request.created_at,
@@ -28,20 +23,15 @@ class githubData {
       labels: pull_request.labels,
       changed_files: this.parseChangedFiles(changedFilesData)
     };
-    console.log(`json: ${json}`);
-
-    return json;
   }
 
   parseChangedFiles(fileResult) {
     let filesChanged = [];
 
     for (let jsonData of fileResult.data) {
-      console.log(`jsonData: ${jsonData}`);
       filesChanged.push(jsonData['filename']);
     }
 
-    console.log(`filesChanged: ${filesChanged}`)
     return filesChanged;
   }
 
