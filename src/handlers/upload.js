7eed0df887970552d1313d7603ff88779002267b
Augusto Queirantes <augusto_queirantes@outlook.com>
2018-09-24 08:00:59 -0300

    Resolve eslint on upload.js

--- src/handlers/upload.js
+++ src/handlers/upload.js
@@ -4,9 +4,9 @@ const githubFile = require(path.resolve(__dirname, '../github/github_data.js'));
 const awsFile = require(path.resolve(__dirname, '../aws/aws_data.js'));
 
 const githubDataParser = new githubFile();
-const awsData = new awsFile();
+const awsData = new awsFile() ;
 
-module.exports.upload = async (event, context) => {
+module.exports.upload = async (event) => {
   const { body: { pull_request, repository } } = event;
 
   let action = event.body.action;
@@ -14,34 +14,16 @@ module.exports.upload = async (event, context) => {
   let owner = pull_request.head.repo.owner.login;
   let repo = repository.name;
   let number = pull_request.number;
-
-  console.log(`action: ${action}`);
-  console.log(`merged: ${merged}`);
-  console.log(`owner: ${owner}`);
-  console.log(`repo: ${repo}`);
-  console.log(`number: ${number}`);
-
   let pullRequestFilesData = await githubDataParser.getPullRequestFiles(owner, repo, number);
   let githubJson = githubDataParser.getPullRequestParsedData(event, pullRequestFilesData);
-  console.log(`githubJson: ${githubJson}`);
-  console.log(`pullRequestFilesData: ${pullRequestFilesData}`);
 
   if (action === 'closed' && merged === true) {
-    let request = await awsData.s3Upload(githubJson, number, repo)
-    console.log(`request: ${request}`);
+    let request = await awsData.s3Upload(githubJson, number, repo);
 
     if (request) {
-      console.log('arquivo gravado');
-
       return {
         request: request
       };
-    } else {
-      console.log('arquivo não gravado');
-
-      return {
-        error: error
-      };
     }
   }
-}
+};
