69f8c59e50f0f11943c6622dc4746878034af6b8
Augusto Queirantes <augusto_queirantes@outlook.com>
2018-09-25 15:22:53 -0300

    Fix github webhook data parsing

--- src/aws/aws_authentication.js
+++ src/aws/aws_authentication.js
@@ -1,9 +1,17 @@
+const log = require('lambda-log');
+
 class awsAuthentication {
   authenticate(AWS){
-    AWS.config.update({
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
-    });
+    try {
+      AWS.config.update({
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+      });
+
+      log.info('Autenticação da AWS ocorreu com sucesso');
+    } catch (error) {
+      log.error(`Autenticação da AWS ocorreu sem sucesso, erro: ${error}`);
+    }
   }
 }
 
