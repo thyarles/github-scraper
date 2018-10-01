5235e618b9658b4f362d3d721117cf9667baf326
Augusto Queirantes <augusto_queirantes@outlook.com>
2018-10-01 20:10:40 -0300

    Fix class method call

--- src/handlers/upload.js
+++ src/handlers/upload.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 const githubFile = require(path.resolve(__dirname, '../github/github_data.js'));
+const getGithuFilesDataFile = require(path.resolve(__dirname, '../github/get_github_data.js'));
 const githubAuthenticationFile = require(path.resolve(__dirname, '../github/github_authentication.js'));
 const awsFile = require(path.resolve(__dirname, '../aws/aws_data.js'));
 const awsAuthenticationFile = require(path.resolve(__dirname, '../aws/aws_authentication.js'));
 
 const githubDataParser = new githubFile();
 const awsData = new awsFile();
+const getGithuFilesData = new getGithuFilesDataFile();
 const githubAuthentication = new githubAuthenticationFile();
 const awsAuthentication = new awsAuthenticationFile();
 
@@ -23,7 +25,7 @@ module.exports.upload = async (event) => {
   let owner = res.pull_request.head.repo.owner.login;
   let repo = res.repository.name;
   let number = res.number;
-  let pullRequestFilesData = await githubDataParser.getPullRequestFiles(octokit, owner, repo, number);
+  let pullRequestFilesData = await getGithuFilesData.getPullRequestFiles(octokit, owner, repo, number);
   let githubJson = await githubDataParser.getPullRequestParsedData(res, pullRequestFilesData);
 
   if (action === 'closed' && merged === true) {
